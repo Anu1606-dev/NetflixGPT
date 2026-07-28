85bd1aa63b7306307c458dcab7f0275757ebcd4f
Anushka Sarkar <sarkaranushka614@gmail.com>
2026-07-28 18:26:36 +0530

    added routing for login and browse pages

---
+++ src/Components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <img src="https://w7.pngwing.com/pngs/572/603/png-transparent-logo-brand-com-student-creative-slate-text-logo-film.png" 
+        alt="Cineverse Logo" 
+        />
+    </div>
+  )
+}
+
+export default Header
